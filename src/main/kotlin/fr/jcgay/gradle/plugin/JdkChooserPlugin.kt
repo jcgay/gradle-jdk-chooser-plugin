76e7d88b6ffd1211d3fd3ef29f3c08557a82ee3e
Jean-Christophe Gay <contact@jeanchristophegay.com>
2020-02-19 22:35:21 +0100

    Apply plugin without using Project#afterEvaluate
    It can lead to really strange behavior like:
    
    Could not resolve all dependencies for configuration ':a:detachedConfiguration3'.
    > A problem occurred configuring project ':b'.
       > Failed to apply plugin [class 'fr.jcgay.gradle.plugin.JdkChooserPlugin']
          > Project#afterEvaluate(Action) on project ':c' cannot be executed in the current context.
    
    It can misbehave when Task Configuration Avoidance is used.

--- src/main/kotlin/fr/jcgay/gradle/plugin/JdkChooserPlugin.kt
+++ src/main/kotlin/fr/jcgay/gradle/plugin/JdkChooserPlugin.kt
@@ -3,40 +3,39 @@ package fr.jcgay.gradle.plugin
 import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.api.tasks.JavaExec
 import org.gradle.api.tasks.compile.JavaCompile
 import org.gradle.api.tasks.testing.Test
+import org.gradle.process.JavaForkOptions
 import java.nio.file.Path
-import java.util.ServiceLoader
+import java.util.*
 
 
 class JdkChooserPlugin: Plugin<Project> {
 
     private val jdkProviders = ServiceLoader.load(JdkProvider::class.java)
-    private var onlyChosenProvider: (JdkProvider) -> Boolean = { true }
 
     override fun apply(project: Project) {
         val extension = project.extensions.create("jdk", JdkChooserExtension::class.java)
 
-        project.afterEvaluate {
-            if (extension.provider != null) {
-                onlyChosenProvider = { it.javaClass.simpleName == extension.provider }
-            }
-
-            project.pluginManager.withPlugin("java") {
-                configureJavaCompilation(project)
-
-                val expectedJavaVersion = findExpectedJavaVersion(project) ?: return@withPlugin
-                configureJavaExecution(project, expectedJavaVersion)
-                configureJavaTest(project, expectedJavaVersion)
-            }
+        project.pluginManager.withPlugin("java") {
+            configureJavaCompilation(project, extension)
+            configureJavaExecution(project, extension)
+            configureJavaTest(project, extension)
         }
     }
 
-    private fun configureJavaTest(project: Project, expectedJavaVersion: JavaVersion) {
-        project.tasks.withType(Test::class.java) { task ->
+    private fun configureJavaTest(project: Project, extension: JdkChooserExtension) {
+        project.tasks.withType(Test::class.java).configureEach { task ->
+            task.doFirst { changeExecutable(project, extension, task) }
+        }
+    }
+
+    private fun changeExecutable(project: Project, extension: JdkChooserExtension, task: JavaForkOptions) {
+        findExpectedJavaVersion(project)?.let { expectedJavaVersion ->
             if (JavaVersion.current() != expectedJavaVersion) {
-                getInstallation(project, expectedJavaVersion)?.let {
+                getInstallation(project, expectedJavaVersion, extension)?.let {
                     project.logger.debug("{} is not the expected Java version {}. Change executable for: {}",
                             JavaVersion.current(), expectedJavaVersion, it)
                     task.executable = "$it/bin/java"
@@ -45,50 +44,37 @@ class JdkChooserPlugin: Plugin<Project> {
         }
     }
 
-    private fun configureJavaExecution(project: Project, expectedJavaVersion: JavaVersion) {
-        project.tasks.withType(JavaExec::class.java) { task ->
-            if (JavaVersion.current() != expectedJavaVersion) {
-                getInstallation(project, expectedJavaVersion)?.let {
-                    project.logger.debug("{} is not the expected Java version {}. Change executable for: {}",
-                            JavaVersion.current(), expectedJavaVersion, it)
-                    task.executable = "$it/bin/java"
-                }
-            }
+    private fun configureJavaExecution(project: Project, extension: JdkChooserExtension) {
+        project.tasks.withType(JavaExec::class.java).configureEach { task ->
+            task.doFirst { changeExecutable(project, extension, task) }
         }
     }
 
-    private fun configureJavaCompilation(project: Project) {
-        project.tasks.withType(JavaCompile::class.java) {
-            val expectedJavaVersion = JavaVersion.toVersion(it.targetCompatibility)
-            if (JavaVersion.current() != expectedJavaVersion) {
-                val installation = getInstallation(project, expectedJavaVersion)
-                if (installation != null) {
-                    project.logger.debug("{} is not the expected Java version {}. Will fork Java compilation with JDK: {}",
-                            JavaVersion.current(), expectedJavaVersion, installation)
-                    it.options.isFork = true
-                    it.options.forkOptions.javaHome = project.file(installation)
-                } else if (JavaVersion.current() >= JavaVersion.VERSION_1_9) {
-                    it.options.compilerArgs.addAll(listOf("--release", expectedJavaVersion.majorVersion))
+    private fun configureJavaCompilation(project: Project, extension: JdkChooserExtension) {
+        project.tasks.withType(JavaCompile::class.java).configureEach {task ->
+            task.doFirst {
+                val expectedJavaVersion = JavaVersion.toVersion(task.targetCompatibility)
+                if (JavaVersion.current() != expectedJavaVersion) {
+                    val installation = getInstallation(project, expectedJavaVersion, extension)
+                    if (installation != null) {
+                        project.logger.debug("{} is not the expected Java version {}. Will fork Java compilation with JDK: {}",
+                                JavaVersion.current(), expectedJavaVersion, installation)
+                        task.options.isFork = true
+                        task.options.forkOptions.javaHome = project.file(installation)
+                    } else if (JavaVersion.current() >= JavaVersion.VERSION_1_9) {
+                        task.options.compilerArgs.addAll(listOf("--release", expectedJavaVersion.majorVersion))
+                    }
                 }
             }
         }
     }
 
     private fun findExpectedJavaVersion(project: Project): JavaVersion? {
-        val expectedJavaVersions = HashSet<String>()
-        project.tasks.withType(JavaCompile::class.java) {
-            expectedJavaVersions.add(it.targetCompatibility)
-        }
-
-        if (expectedJavaVersions.size > 1) {
-            return null
-        }
-
-        return JavaVersion.toVersion(expectedJavaVersions.first())
+        return project.extensions.findByType(JavaPluginExtension::class.java)?.targetCompatibility
     }
 
-    private fun getInstallation(project: Project, expectedJavaVersion: JavaVersion): Path? =
-            jdkProviders.filter(onlyChosenProvider)
+    private fun getInstallation(project: Project, expectedJavaVersion: JavaVersion, extension: JdkChooserExtension): Path? =
+            jdkProviders.filter { it.javaClass.simpleName == extension.provider }
                     .map { it.findInstallation(expectedJavaVersion, project) }
                     .firstOrNull { it != null }
 
